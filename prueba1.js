bfdbd9ddd72d8c06c667f8e16fcc567d3b75d360
Paula <mpcastanedagarcia@gmail.com>
2023-03-18 13:27:34 -0500

    cree rama

---
+++ prueba1.js
@@ -0,0 +1,39 @@
+// project1
+let contactos= ["mariana lopez", "fernando diaz","salome castañeda" ]
+
+function añadir(contacto) {
+  contactos.unshift(contacto);
+}
+
+function borrar(contacto) {
+  contactos = contactos.filter(persona => persona !== contacto);
+}
+
+
+
+function imprimir() {
+  console.log("contactos:");
+  contactos.forEach(function (contacto) {
+    console.log(contacto);
+  });
+}
+imprimir();
+borrar("fernando diaz")
+imprimir();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

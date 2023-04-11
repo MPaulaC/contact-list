91b90e690765ca2f678aa9a5a0be6ba6656590d2
Paula <mpcastanedagarcia@gmail.com>
2023-04-10 23:49:03 -0500

    escribi cambios en rama sobre la lista de contactos

--- prueba1.js
+++ prueba1.js
@@ -1,3 +1,45 @@
+// Crea una lista de contactos con datos predefinidos
+let contactList = [
+  {
+    id: 1,
+    nombres: "Mariana",
+    apellidos: "Lopez",
+    teléfono: "1234",
+    ubicaciones: "Colombia",
+    ciudad: "Palmira",
+    dirección: "carrera 40# 70-89"
+  },
+  {
+    id: 2,
+    nombres: "Fernando",
+    apellidos: "Diaz",
+    teléfono: "897655",
+    ubicaciones: "Colombia",
+    ciudad: "Palmira",
+    dirección: "Carre 45 #9a-87"
+    },
+    {
+        id: 3,
+        nombres: "Salome",
+        apellidos: "Castañeda",
+        teléfono: "235187",
+        ubicaciones: "Colombia",
+        ciudad: "Palmira",
+        dirección: "Ccalle 44 #b4-45"
+    }
+];
+
+// Función para añadir un nuevo contacto a la lista
+function addContact(newContact) {
+  contactList.push(newContact);
+}
+
+// Función para borrar un contacto existente de la lista
+function deleteContact(id) {
+  contactList = contactList.filter(function(contact) {
+    return contact.id !== id;
+  });
+}
 
  
 
